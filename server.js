791ab4974e54f02e32201c79274ce91b6d9ca74e
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-10-30 09:50:08 -0400

    connected to mongo database

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://mcf:<Database123>@cluster0.alndg.mongodb.net/<MorganOnPage>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,
+  useUnifiedTopology: true
+ });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
+
+
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.listen(3000, function() {
